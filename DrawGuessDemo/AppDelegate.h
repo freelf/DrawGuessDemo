d63e03c6b0e796b85835e453695b895af0c54ea6
Freelf <zdp930128@gmail.com>
2019-05-27 19:27:44 +0800

    Initial Commit

---
+++ DrawGuessDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawGuessDemo
+//
+//  Created by Beyond on 2019/5/27.
+//  Copyright © 2019 Freelf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawGuessDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawGuessDemo
+//
+//  Created by Beyond on 2019/5/27.
+//  Copyright © 2019 Freelf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
